e1ac3efb8bc38f8f45f26d5f97c06741cb3e6e05
MetaFight <5166127+MetaFight@users.noreply.github.com>
2022-12-04 11:42:24 +0000

    shrinkify

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-
-bool isInTestMode = false;
+﻿bool isInTestMode = false;
 
 var input =
     isInTestMode
@@ -9,47 +7,17 @@ var input =
 
 var data =
     File.ReadAllLines(input)
-        .Select(line => line.Split(new[] { '-', ',' }, StringSplitOptions.TrimEntries).Select(item => int.Parse(item)).Chunk(2))
-        .Select(pair => pair.Select(item => (from: item[0], to: item[1])))
+        .Select(line =>
+            line.Split(new[] { '-', ',' }, StringSplitOptions.TrimEntries)
+                .Select(item => int.Parse(item))
+                .Chunk(2)
+                .Select(item => (from: item[0], to: item[1]))
+                .OrderBy(item => item.from).ThenByDescending(item => item.to).ToList())
         .ToList();
-        
 
 
-// Part 1
-{
-    var part1 =
-        data.Select(pair => pair.OrderBy(item => item.from).ThenByDescending(item => item.to).ToList())
-            .Where(pair => pair[0].from <= pair[1].from && pair[1].to <= pair[0].to)
-            .Count();
-    Console.WriteLine($"Part 1: {part1}");
-}
+var part1 = data.Where(pair => pair[0].from <= pair[1].from && pair[1].to <= pair[0].to).Count();
+Console.WriteLine($"Part 1: {part1}");
 
-
-// Part 2
-{
-    var part2 =
-        data.Select(pair => pair.OrderBy(item => item.from).ThenByDescending(item => item.to).ToList())
-            .Where(pair => pair[0].to >= pair[1].from)
-            .Count();
-    Console.WriteLine($"Part 1: {part2}");
-}
-
-static void print(IEnumerable source, int indent = 0)
-{
-    var padding = new string(' ', indent);
-    foreach (var item in source)
-    {
-        if (item is not string && item is IEnumerable nested)
-        {
-            Console.WriteLine(padding + ">");
-            print(nested, indent + 2);
-            Console.WriteLine(padding + "<");
-        }
-        else
-        {
-            Console.WriteLine(
-                padding +
-                item.ToString().PadLeft(indent));
-        }
-    }
-}
+var part2 = data.Where(pair => pair[0].to >= pair[1].from).Count();
+Console.WriteLine($"Part 2: {part2}");
