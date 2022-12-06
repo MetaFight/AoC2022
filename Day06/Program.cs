ceade0426515471e9e897bc97ae962f53a9f76f4
MetaFight <5166127+MetaFight@users.noreply.github.com>
2022-12-06 10:55:17 +0000

    Day 06.

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -5,17 +5,38 @@ var input =
         ? "input-testing.txt"
         : "input.txt";
 
-var data =
-    File.ReadAllLines(input);
+var data = File.ReadAllText(input);
+const int PacketStartMarkerSize = 4;
 
 // Part 1
 {
-    var part1 = 0;
+    var part1 = IndexOfMarker(data, PacketStartMarkerSize);
     Console.WriteLine($"Part 1: {part1}");
 }
 
 // Part 2
 {
-    var part2 = 0;
+    const int MessageStartMarkerSize = 14;
+
+    var markerIndex = IndexOfMarker(data, PacketStartMarkerSize);
+    var part2 = markerIndex + IndexOfMarker(data.Substring(markerIndex), MessageStartMarkerSize);
     Console.WriteLine($"Part 2: {part2}");
+}
+
+static int IndexOfMarker(string data, int windowSize)
+{
+    int windowStart;
+    for (windowStart = 0; windowStart < data.Length - windowSize; windowStart++)
+    {
+        var isMarker =
+            data.Substring(windowStart, windowSize)
+                .Distinct()
+                .Count() == windowSize;
+        if (isMarker)
+        {
+            return windowStart + windowSize;
+        }
+    }
+
+    return -1;
 }
