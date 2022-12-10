895731bfa21326d780b4a02cb6514a6696824958
MetaFight <5166127+MetaFight@users.noreply.github.com>
2022-12-10 11:34:34 +0000

    Day 10

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,21 +1,103 @@
-﻿bool isInTestMode = true;
+﻿using System.Text;
+
+bool isInTestMode = false;
 
 var input =
     isInTestMode
         ? "input-testing.txt"
         : "input.txt";
 
-var data =
-    File.ReadAllLines(input);
+var data = File.ReadAllLines(input);
+
+var steps =
+    data.Select(item => item.Split())
+        .Select<string[], Action<CommLink>>(row => row switch
+        {
+            ["noop"] => (CommLink x) => x.NoOp(),
+            ["addx", var arg] => (CommLink x) => x.AddX(int.Parse(arg)),
+            _ => throw new NotImplementedException()
+        })
+        .ToList();
+
+var commLink = new CommLink(20, 60, 100, 140, 180, 220);
+
+foreach (var step in steps)
+{
+    step(commLink);
+}
 
 // Part 1
 {
-    var part1 = 0;
-    Console.WriteLine($"Part 1: {part1}");
+    Console.WriteLine($"Part 1: {commLink.SignalStrength}");
 }
 
 // Part 2
 {
-    var part2 = 0;
-    Console.WriteLine($"Part 2: {part2}");
+    Console.WriteLine($"Part 2: {commLink.Crt}");
+}
+
+class CommLink
+{
+    private readonly Queue<int> inspectionCycles;
+
+    private int counter;
+    private int x;
+    private StringBuilder crt;
+
+    public CommLink(params int[] inspectionCycles)
+    {
+        this.inspectionCycles = new Queue<int>(inspectionCycles.OrderBy(item => item));
+
+        this.counter = 0;
+        this.x = 1;
+        this.crt = new StringBuilder();
+    }
+
+    public int SignalStrength { get; private set; }
+
+    public string Crt => this.crt.ToString();
+
+    public void AddX(int value)
+    {
+        this.Spin(2);
+        this.x += value;
+    }
+
+    public void NoOp() => this.Spin(1);
+
+    private void Spin(int count)
+    {
+        while (count > 0)
+        {
+            Part2();
+
+            this.counter++;
+            count--;
+
+            Part1();
+        }
+
+        void Part1()
+        {
+            if (this.inspectionCycles.Count > 0 && this.counter == this.inspectionCycles.Peek())
+            {
+                this.SignalStrength += this.x * this.counter;
+                this.inspectionCycles.Dequeue();
+            }
+        }
+
+        void Part2()
+        {
+            var position = this.counter % 40;
+            if (position == 0)
+            {
+                this.crt.AppendLine();
+            }
+
+            var spriteRelativeToBeam = this.x - position;
+            var isSpritePixel = -1 <= spriteRelativeToBeam && spriteRelativeToBeam <= 1;
+
+            this.crt.Append(isSpritePixel ? '#' : '.');
+        }
+    }
 }
